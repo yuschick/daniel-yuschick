b7beab9e02271471f0121c8f3e6f009cfce44087
Daniel Yuschick <danyuschick@gmail.com>
2020-05-15 21:10:01 +0300

    Simplify about content

---
+++ src/components/About/content.ts
@@ -0,0 +1,45 @@
+const data = [
+  {
+    title: "Born and raised in Pennsylvania, USA",
+    content: [
+      "Spent my 7th birthday singing every word at an Alice Cooper concert",
+      "Worked two years in a record store",
+      "Freelanced as a web designer and developer",
+      "Played drums in multiple metal bands",
+    ],
+  },
+  {
+    title: "Moved to North Carolina, USA in 2009",
+    content: [
+      "Learned what a recession was",
+      "Continued to freelance",
+      "Never used my Bachelor's Degree in Criminal Justice / Computer Forensics",
+      "Adopted my dog, Abbie",
+      "Competed in several YMCA basketball leagues",
+      "Tried bean-to-bar chocolate and have been spoiled since",
+      "Taught Frontend Engineering at The Iron Yard coding bootcamp",
+    ],
+  },
+  {
+    title: "Followed my love of winter and metal to Helsinki, Finland in 2017",
+    content: [
+      "Lived in an AirBnB without running water for the first two weeks",
+      "A lot more basketball and a lot less freelancing",
+      "Discovered my love of traveling",
+      "Wrote my first novel, The Mines",
+      "Attended Turkmenistan's annual Independence Day horse race",
+      "Returned to indoor rock climbing",
+      "Learned what a pandemic was",
+    ],
+  },
+  {
+    title: "Still to come...",
+    content: [
+      "Write my non-sequel second novel",
+      "Climb a mountain in Antarctica",
+      "Find a way to combine development with my relentless urge to explore",
+    ],
+  },
+]
+
+export default data
