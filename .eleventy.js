a1bc5811c47339937539fc048d3bd7c28ab0362d
Daniel Yuschick <daniel.yuschick@gmail.com>
2022-06-12 11:36:31 +0300

    cleaning up about page

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+const CleanCSS = require("clean-css");
+
 module.exports = (config) => {
-  config.addPassthroughCopy("./src/styles");
   config.addNunjucksGlobal("getYear", function () {
     return new Date().getFullYear();
   });
+
+  config.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  config.addPassthroughCopy("./src/assets");
+  config.addPassthroughCopy("./src/styles");
 
   return {
     dir: {
